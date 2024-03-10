ec72438e73bb3c63b637538442d8eda2100a9c19
abdallah <Abdallah@local.com>
2024-03-10 19:35:38 +0200

    abdallah

--- src/config.js
+++ src/config.js
@@ -1,15 +1,17 @@
 import pg from "pg";
 const pool = new pg.Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "Api",
-  password: "11620000",
+  user: "abdallah",
+  host: "dpg-cn9p3rv79t8c73c51vd0-a.oregon-postgres.render.com",
+  database: "WinIt",
+  password: "NjAo5XQyVpWJNz2KCMPIMDktWzQOLizx",
   port: 5432,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  //   ssl: {
-  //     rejectUnauthorized: false,
-  //   },
+  max: 20, // Adjust as needed
+  idleTimeoutMillis: 30000, // Adjust as needed
+  ssl: {
+    // These options are often required for connecting to a remote PostgreSQL server
+    // Adjust them based on your server's configuration
+    rejectUnauthorized: false,
+  },
 });
 export const secret = "J3mlt0WzyktgiyXiRff5ua883P9t7jM2";
 export const saltRounds = 10;
