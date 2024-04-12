f6e72da35387139510a80fc9b42a2010f92367b9
abdallah <Abdallah@local.com>
2024-04-12 17:24:23 +0200

    abdallah

--- src/Products/router.js
+++ src/Products/router.js
@@ -9,6 +9,6 @@ import { upload } from "../files/files.js";
 export const ProductsRouter = express.Router();
 
 ProductsRouter.post("/get_products", get_products);
-//ProductsRouter.post("/upsert_product", upload.single("image"), upsert_product);
+ProductsRouter.post("/upsert_product", upload.single("image"), upsert_product);
 ProductsRouter.post("/delete_product", delete_product);
 ProductsRouter.post("/campaign_products", campaign_products);
